468f4587d13a58f587b987d9947250221ad0732e
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2022-05-23 17:18:01 +0200

    add local storage storing functionality

--- src/context/TasksContext.js
+++ src/context/TasksContext.js
@@ -2,68 +2,69 @@ import { createContext, useReducer } from "react";
 
 // initial state
 const appState = {
-  tasks: [
-    {
-      id: 1,
-      title: "code",
-      description: "do refilex task for Todo application",
-      status: "todo",
-    },
-    {
-      id: 2,
-      title: "solve",
-      description: "solve Leetcode problem ",
-      status: "todo",
-    },
-    {
-      id: 3,
-      title: "train",
-      description: "go to train",
-      status: "todo",
-    },
+  // tasks: [
+  //   {
+  //     id: 1,
+  //     title: "code",
+  //     description: "do refilex task for Todo application",
+  //     status: "todo",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "solve",
+  //     description: "solve Leetcode problem ",
+  //     status: "todo",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "train",
+  //     description: "go to train",
+  //     status: "todo",
+  //   },
 
-    /************** In Progress Tasks ************/
+  //   /************** In Progress Tasks ************/
 
-    {
-      id: 4,
-      title: "code in progress",
-      description: "do refilex task for Todo application",
-      status: "inProgress",
-    },
-    {
-      id: 5,
-      title: "solve in progress",
-      description: "solve Leetcode problem ",
-      status: "inProgress",
-    },
-    {
-      id: 6,
-      title: "train in progress",
-      description: "go to train",
-      status: "inProgress",
-    },
+  //   {
+  //     id: 4,
+  //     title: "code in progress",
+  //     description: "do refilex task for Todo application",
+  //     status: "inProgress",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "solve in progress",
+  //     description: "solve Leetcode problem ",
+  //     status: "inProgress",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "train in progress",
+  //     description: "go to train",
+  //     status: "inProgress",
+  //   },
 
-    /************** Done Tasks ************/
+  //   /************** Done Tasks ************/
 
-    {
-      id: 7,
-      title: "code done",
-      description: "do refilex task for Todo application",
-      status: "done",
-    },
-    {
-      id: 8,
-      title: "solve done",
-      description: "solve Leetcode problem ",
-      status: "done",
-    },
-    {
-      id: 9,
-      title: "train done",
-      description: "go to train",
-      status: "done",
-    },
-  ],
+  //   {
+  //     id: 7,
+  //     title: "code done",
+  //     description: "do refilex task for Todo application",
+  //     status: "done",
+  //   },
+  //   {
+  //     id: 8,
+  //     title: "solve done",
+  //     description: "solve Leetcode problem ",
+  //     status: "done",
+  //   },
+  //   {
+  //     id: 9,
+  //     title: "train done",
+  //     description: "go to train",
+  //     status: "done",
+  //   },
+  // ],
+  tasks: JSON.parse(localStorage.getItem("tasks")) || [],
   movingTask: {},
 };
 
@@ -71,6 +72,10 @@ const appState = {
 const appReducer = (state, action) => {
   switch (action.type) {
     case "ADD_NEW_TASK":
+      localStorage.setItem(
+        "tasks",
+        JSON.stringify([...state.tasks, action.payload])
+      );
       return {
         ...state,
         tasks: [...state.tasks, action.payload],
@@ -83,6 +88,13 @@ const appReducer = (state, action) => {
       };
 
     case "UPDATE_TASKS_AFTER_DRAG":
+      localStorage.setItem(
+        "tasks",
+        JSON.stringify([
+          ...state.tasks.filter((task) => task.id !== action.payload),
+          state.movingTask,
+        ])
+      );
       return {
         ...state,
         tasks: [
@@ -92,6 +104,13 @@ const appReducer = (state, action) => {
       };
 
     case "UPDATE_TASKS_AFTER_EDIT":
+      localStorage.setItem(
+        "tasks",
+        JSON.stringify([
+          ...state.tasks.filter((task) => task.id !== action.payload.id),
+          action.payload,
+        ])
+      );
       return {
         ...state,
         tasks: [

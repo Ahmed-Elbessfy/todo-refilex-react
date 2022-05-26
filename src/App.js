10893d3a04068c7064c0001d631d65f7f3162a0e
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2022-05-26 19:45:14 +0200

    try to fix no show data on page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { AppProvider } from "./context/TasksContext";
 
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 function App() {
   return (
     <AppProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </AppProvider>
   );
 }

947d0dd689f8a2b3210bb372eb1949c4f2deec7a
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2022-05-26 19:24:33 +0200

    style About page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,22 +2,30 @@ import React from "react";
 import Header from "../components/Header";
 
 const About = () => {
+  // style variables
+  const liStyle = {
+    display: "list-item",
+    minHeight: "auto",
+    listStyle: "decimal",
+  };
   return (
     <>
       <Header />
       <main className="App">
         <section>
-          <h1>TODO application</h1>
-          <p>This a todo application as a part of hiring process</p>
-          <h2>Features you can find:</h2>
+          <h1 style={{ marginBottom: "1rem" }}>TODO application</h1>
+          <p>This is a todo application as a part of hiring process</p>
+          <h2 style={{ textAlign: "left", margin: "2rem 0 1rem" }}>
+            Features you can find:
+          </h2>
           <ol>
-            <li>Add, edit and delete tasks</li>
-            <li>
+            <li style={liStyle}>Add, edit and delete tasks</li>
+            <li style={liStyle}>
               You can move tasks from Todo to in progress and vice verse but
               once task is done it can't be moved
             </li>
-            <li>You can move tasks by drag and drop</li>
-            <li>Store your tasks in local storage</li>
+            <li style={liStyle}>You can move tasks by drag and drop</li>
+            <li style={liStyle}>Store your tasks in local storage</li>
           </ol>
         </section>
       </main>
